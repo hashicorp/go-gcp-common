4b38f46ac60aebc9c67f2548aec1ff5bc1e9a4eb
Emily Ye <emilyye@google.com>
2018-02-23 11:15:17 -0800

    add resourceName helpers

--- gcputil/iam_admin.go
+++ gcputil/iam_admin.go
@@ -16,18 +16,25 @@ type ServiceAccountId struct {
 	EmailOrId string
 }
 
+func (id *ServiceAccountId) ResourceName() string {
+	return fmt.Sprintf(ServiceAccountTemplate, id.Project, id.EmailOrId)
+}
+
 type ServiceAccountKeyId struct {
 	Project   string
 	EmailOrId string
 	Key       string
 }
 
+func (id *ServiceAccountKeyId) ResourceName() string {
+	return fmt.Sprintf(ServiceAccountKeyTemplate, id.Project, id.EmailOrId, id.Key)
+}
+
 // ServiceAccount wraps a call to the GCP IAM API to get a service account.
 func ServiceAccount(iamClient *iam.Service, accountId *ServiceAccountId) (*iam.ServiceAccount, error) {
-	accountResource := fmt.Sprintf(ServiceAccountTemplate, accountId.Project, accountId.EmailOrId)
-	account, err := iamClient.Projects.ServiceAccounts.Get(accountResource).Do()
+	account, err := iamClient.Projects.ServiceAccounts.Get(accountId.ResourceName()).Do()
 	if err != nil {
-		return nil, fmt.Errorf("could not find service account '%s': %v", accountResource, err)
+		return nil, fmt.Errorf("could not find service account '%s': %v", accountId.ResourceName(), err)
 	}
 
 	return account, nil
@@ -35,8 +42,8 @@ func ServiceAccount(iamClient *iam.Service, accountId *ServiceAccountId) (*iam.S
 
 // ServiceAccountKey wraps a call to the GCP IAM API to get a service account key.
 func ServiceAccountKey(iamClient *iam.Service, keyId *ServiceAccountKeyId) (*iam.ServiceAccountKey, error) {
-	keyResource := fmt.Sprintf(ServiceAccountKeyTemplate, keyId.Project, keyId.EmailOrId, keyId.Key)
-	key, err := iamClient.Projects.ServiceAccounts.Keys.Get(keyResource).PublicKeyType(ServiceAccountKeyFileType).Do()
+	keyResource := keyId.ResourceName()
+	key, err := iamClient.Projects.ServiceAccounts.Keys.Get(keyId.ResourceName()).PublicKeyType(ServiceAccountKeyFileType).Do()
 	if err != nil {
 		return nil, fmt.Errorf("could not find service account key '%s': %v", keyResource, err)
 	}
